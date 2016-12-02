7f5961f48a5e8640ea3d3d44fa6c8d9589918c2b
Matt Kraatz <Matt.Kraatz@gmail.com>
2016-12-02 13:40:54 -0600

    added site js

--- src/user-auth/wwwroot/js/site.js
+++ src/user-auth/wwwroot/js/site.js
@@ -1,1 +1,49 @@
 ﻿// Write your Javascript code.
+// Write your Javascript code.
+$(document).ready(function () {
+
+    // update user selected on server
+    $("#ChosenUser_UserId").on("change", function () {
+        $.ajax({
+            url: `/User/Activate/${$(this).val()}`,
+            method: "POST",
+            dataType: "json",
+            contentType: 'application/json; charset=utf-8'
+        }).done(() => {
+            location.replace("/");
+        });
+    });
+
+    // update subtype for product on server
+    $("#NewProduct_ProductTypeId").on("change", function () {
+        if ($("#NewProduct_ProductTypeId").val() > 0) {
+            $('#NewProduct_ProductSubTypeId').val(0);
+            this.form.submit();
+        }
+    });
+
+    // update subtype for product on server
+    $("#CurrentProduct_ProductTypeId").on("change", function () {
+        if ($("#CurrentProduct_ProductTypeId").val() > 0) {
+            $('#CurrentProduct_ProductSubTypeId').val(0);
+            this.form.submit();
+        }
+    });
+
+    // disable the Checkout button until a PaymentType has been selected
+    if ($("#selectedPaymentId").val() == 0) {
+        $("#checkoutButton").prop("disabled", true);
+    }
+    else {
+        $("#checkoutButton").removeAttr("disabled");
+    }
+    $("#selectedPaymentId").on("change", function () {
+        if ($("#selectedPaymentId").val() > 0) {
+            $("#checkoutButton").removeAttr("disabled");
+        }
+        else if ($("#selectedPaymentId").val() == 0) {
+            $("#checkoutButton").prop("disabled", true);
+        }
+    })
+
+});
